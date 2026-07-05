e86692825892b90636065ab1ac1c4c61e85bca73
Shaikh-Danish <shaikhsaalim327@gmail.com>
2026-07-05 17:09:32 +0530

    feat: add Docker support with multi-stage build and CI/CD deployment pipeline

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "standalone",
   experimental: {
     proxyClientMaxBodySize: "50mb",
     staleTimes: {
